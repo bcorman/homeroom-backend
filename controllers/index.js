d6a13e530d81026d6bafba50cc1b93f41e1e480b
bcorman <benjamincorman@gmail.com>
2018-07-05 14:42:38 -0700

    added controller index

---
+++ controllers/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  assignment: require('./assignmentController.js'),
+  class: require('./classController.js'),
+  grade: require('./gradeController.js'),
+  post: require('./postController.js'),
+  student: require('./studentController.js'),
+  unit: require('./unitController.js'),
+  user: require('./userController.js')
+}
