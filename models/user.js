a7bf20ae2812b74d267586ebdd62a8d6b5cafefb
bcorman <benjamincorman@gmail.com>
2018-07-06 11:27:08 -0700

    added bcrypt hashing methods to UserSchema

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
+const bcrypt = require('bcrypt-nodejs')
 
 const UserSchema = new Schema({
   userName: {
@@ -19,6 +20,28 @@ const UserSchema = new Schema({
     ref: 'Class'
   }]
 })
+
+UserSchema.pre('save', (next) => {
+  const user = this
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) { return next(err) }
+
+    bcrypt.hash(user.password, salt, null, (err, hash) => {
+      if (err) { return next(err) }
+
+      user.password = hash;
+      next()
+    })
+  })
+})
+
+UserSchema.methods.comparePassword = (candidatePassword, callback) => {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) { return callback(err) }
+    callback(null, isMatch)
+  })
+}
+
 
 const User = mongoose.model('User', UserSchema)
 
